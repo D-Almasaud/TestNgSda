6aea1150fee5d4b3d09040882efc896aaf26f25b
D-almasaud <you@example.com>
2024-03-18 12:14:32 +0300

    day18 commit

---
+++ src/test/java/sda/tests/Day18/C01ParallelTest01.java
@@ -0,0 +1,51 @@
+package sda.tests.Day18;
+
+import org.testng.annotations.Test;
+import utilities.TestBase;
+
+public class C01ParallelTest01  {
+    @Test
+    public void Test01(){
+      String threadsName =Thread.currentThread().getName();
+        System.out.println("Thread executing for test 01"+threadsName);
+    }
+    @Test
+    public void Test02(){
+        String threadsName =Thread.currentThread().getName();
+        System.out.println("Thread executing for test 02"+threadsName);
+    }
+    @Test
+    public void Test03(){
+        String threadsName =Thread.currentThread().getName();
+        System.out.println("Thread executing for test 03"+threadsName);
+    }
+    @Test
+    public void Test04(){
+        String threadsName =Thread.currentThread().getName();
+        System.out.println("Thread executing for test 04"+threadsName);
+    }
+
+    @Test
+    public void Test05(){
+        String threadsName =Thread.currentThread().getName();
+        System.out.println("Thread executing for test 05"+threadsName);
+    }
+    @Test
+    public void Test06(){
+        String threadsName =Thread.currentThread().getName();
+        System.out.println("Thread executing for test 06"+threadsName);
+    }
+
+    @Test
+    public void Test07(){
+        String threadsName =Thread.currentThread().getName();
+        System.out.println("Thread executing for test 07"+threadsName);
+    }
+    @Test
+    public void Test08(){
+        String threadsName =Thread.currentThread().getName();
+        System.out.println("Thread executing for test 08"+threadsName);
+    }
+
+
+}
---
+++ src/test/java/sda/tests/Day18/C02ParallelTest02.java
@@ -0,0 +1,31 @@
+package sda.tests.Day18;
+
+import org.testng.annotations.Test;
+import utilities.TestBase;
+
+
+
+public class C02ParallelTest02 extends TestBase {
+
+    @Test
+    public void test01(){
+
+        driver.get("https://www.clarusway.com");
+    }
+    @Test
+    public void test02(){
+        driver.get("https://www.ebay.com");
+
+
+    }
+    @Test
+    public void test03(){
+       driver.get("https://www.linkedin.com");
+
+    }
+    @Test
+    public void test04(){
+       driver.get("https://www.google.com");
+
+    }
+}
